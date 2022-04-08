bca67199b378079c712cff8b682710bb0c6dab74
Christian <blankj.me@gmail.com>
2022-04-08 23:48:08 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+import Gallery from './Gallery'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <h1>stack underflow</h1>
+      <p className='intro-text'>We try our best to answer all questions inside our FAQ</p>
+      <p className='intro-text'>Click on the Button below for careful selected Developer Advice</p>
+      <Gallery></Gallery>
+    </div>
+  )
+}
+
+export default App
